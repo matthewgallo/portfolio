955586161adbb2a3ff5dac05a2ea4253045ba94e
Matthew Gallo <mdgallo@us.ibm.com>
2017-07-28 22:39:59 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+export default App;
