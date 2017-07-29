68877ed5f3458b9ed98103e1f15fd500b000787e
Matthew Gallo <mdgallo@us.ibm.com>
2017-07-29 12:00:22 -0500

    Add footer and footer link components and classes for font weights.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
+import FooterLink from './FooterLink';
 
 //styles
 import './footer.scss';
 
-class App extends Component {
+class Footer extends Component {
   render() {
     return (
-      <div className="footer-container">
-      </div>
+			<footer>
+	      <div className="footer-container">
+					<span className="footer-logo mg-h2 mg-light">MG</span>
+					<ul className="footer-link-container">
+						<FooterLink
+							url="https://twitter.com/matthewgallo27"
+							name="Twitter"
+						/>
+						<FooterLink
+							url="https://www.linkedin.com/in/matthew-gallo-1090748a/"
+							name="LinkedIn"
+						/>
+						<FooterLink
+							url="https://github.com/matthewgallo/"
+							name="Github"
+						/>
+					</ul>
+	      </div>
+			</footer>
     )
   }
 }
 
-export default App;
+export default Footer;
---
+++ src/components/FooterLink.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+//styles
+import './footerLink.scss';
+
+class FooterLink extends Component {
+  render() {
+    return (
+      <li className="footer-link-item">
+				<a href={this.props.url} className="footer-link">{this.props.name}</a>
+      </li>
+    )
+  }
+}
+
+FooterLink.propTypes = {
+		url: PropTypes.string.isRequired,
+		name:PropTypes.string.isRequired,
+}
+
+export default FooterLink;
