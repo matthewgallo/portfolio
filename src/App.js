7633fe1bedb0fcef670d6c812028d532f459e2ae
Matthew Gallo <mdgallo@us.ibm.com>
2017-08-08 22:00:51 -0500

    Added work and workItem component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Work from './components/Work';
 
 //styles
 import './App.scss';
@@ -12,11 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-				<Nav />
-				<Header />
-				<Footer />
+        <Nav />
+        <Header />
+        <Work />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
