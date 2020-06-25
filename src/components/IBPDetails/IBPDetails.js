e7d23e286ad479e2af10d96b6584c13b4f6fd226
Matt Gallo <mdgallo@us.ibm.com>
2020-06-25 16:23:09 -0400

    Testing linting setup.

--- src/components/IBPDetails/IBPDetails.js
+++ src/components/IBPDetails/IBPDetails.js
@@ -2,19 +2,19 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useSpring } from 'react-spring';
 import DetailHeader from '../DetailPage/DetailHeader';
 import IBPheaderImg from '../../assets/images/ibp_details_header.svg';
-import SectionMaxWidth from '../../components/SectionMaxWidth/SectionMaxWidth';
-import Text from '../../components/Text/Text';
-import Tag from '../../components/Tag/Tag';
-import GradientLink from '../../components/GradientLink/GradientLink';
+import SectionMaxWidth from '../SectionMaxWidth/SectionMaxWidth';
+import Text from '../Text/Text';
+import Tag from '../Tag/Tag';
+import GradientLink from '../GradientLink/GradientLink';
 import ibpVideo from '../../assets/images/blockchain/ibp_screenshots.mp4';
 import ToggleIcon from './ToggleIcon';
 import reactlogo from '../../assets/images/blockchain/react.svg';
 import reduxlogo from '../../assets/images/blockchain/redux.svg';
 import sasslogo from '../../assets/images/blockchain/sass.svg';
 import hyperledgerlogo from '../../assets/images/blockchain/hyperledger_icon.svg';
-import FooterLogoLink from '../../components/FooterLogoLink/FooterLogoLink';
-import './ibpDetails.scss';
+import FooterLogoLink from '../FooterLogoLink/FooterLogoLink';
 
+import './ibpDetails.scss';
 
 const BlockchainPage = () => {
 	const videoRef = useRef();
@@ -40,12 +40,12 @@ const BlockchainPage = () => {
 	};
 
 	const tagAnimation = useSpring({
-		to: async (next) => {
+		to: async next => {
 			await next({
 				opacity: 1,
 				transform: 'translateX(0)',
 				delay: 200,
-				config: { duration: 400, },
+				config: { duration: 400 },
 			});
 		},
 		from: {
@@ -54,13 +54,13 @@ const BlockchainPage = () => {
 		},
 	});
 	const tagAnimation2 = useSpring({
-		to: async (next) => {
+		to: async next => {
 			await next({
 				opacity: 1,
 				transform: 'translateX(0)',
 				delay: 250,
-				config: { duration: 400 }
-			})
+				config: { duration: 400 },
+			});
 		},
 		from: {
 			opacity: 0,
@@ -68,13 +68,13 @@ const BlockchainPage = () => {
 		},
 	});
 	const tagAnimation3 = useSpring({
-		to: async (next) => {
+		to: async next => {
 			await next({
 				opacity: 1,
 				transform: 'translateX(0)',
 				delay: 300,
-				config: { duration: 400 }
-			})
+				config: { duration: 400 },
+			});
 		},
 		from: {
 			opacity: 0,
@@ -86,35 +86,71 @@ const BlockchainPage = () => {
 		<>
 			<DetailHeader
 				title="IBM Blockchain Platform"
-				subtitle={<>
-					#1 ranked Blockchain platform by{' '}
-						<GradientLink
-							external
-							url="https://www.juniperresearch.com/press/press-releases/ibm-ranked-no-1-blockchain-technology-leader"
-						>
+				subtitle={
+					<>
+						#1 ranked Blockchain platform by{' '}
+						<GradientLink external
+url="https://www.juniperresearch.com/press/press-releases/ibm-ranked-no-1-blockchain-technology-leader">
 							Juniper Research
-						</GradientLink>
-						{' '}and {' '}
-						<GradientLink
-							external
-							url="https://www.ibm.com/account/reg/us-en/signup?formid=urx-37667"
-						>
+						</GradientLink>{' '}
+						and{' '}
+						<GradientLink external
+url="https://www.ibm.com/account/reg/us-en/signup?formid=urx-37667">
 							HFS Research
 						</GradientLink>
-				</>}
-				tags={<>
-					<Tag style={tagAnimation}><span style={{marginRight: '.5rem'}} role="img" aria-label="Crane emoji for Front end development">🏗️</span> Front end development*</Tag>
-					<Tag style={tagAnimation2}><span style={{marginRight: '.5rem'}} role="img" aria-label="Magnifying glass emoji for user research">🔍</span> User research</Tag>
-					<Tag style={tagAnimation3}><span style={{marginRight: '.5rem'}} role="img" aria-label="Tools emoji for user experience design">🛠️</span> UX design</Tag>
-				</>}
+					</>
+				}
+				tags={
+					<>
+						<Tag style={tagAnimation}>
+							<span
+								style={{
+									marginRight: '.5rem',
+								}}
+								role="img"
+								aria-label="Crane emoji for Front end development"
+							>
+								🏗️
+							</span>{' '}
+							Front end development*
+						</Tag>
+						<Tag style={tagAnimation2}>
+							<span
+								style={{
+									marginRight: '.5rem',
+								}}
+								role="img"
+								aria-label="Magnifying glass emoji for user research"
+							>
+								🔍
+							</span>{' '}
+							User research
+						</Tag>
+						<Tag style={tagAnimation3}>
+							<span
+								style={{
+									marginRight: '.5rem',
+								}}
+								role="img"
+								aria-label="Tools emoji for user experience design"
+							>
+								🛠️
+							</span>{' '}
+							UX design
+						</Tag>
+					</>
+				}
 				headerImage={{
 					src: IBPheaderImg,
-					alt: 'IBM Blockchain Platform screenshot'
+					alt: 'IBM Blockchain Platform screenshot',
 				}}
 			/>
 			<div className="project--call-out">
 				<SectionMaxWidth>
-					<h4 className="project-call-out-header">The <strong>next generation</strong> of blockchain for business. Proven, flexible and built to run on <strong>any</strong> cloud. Deploy the leading Hyperledger Fabric platform in the environment that’s right for <strong>your</strong> enterprise.</h4>
+					<h4 className="project-call-out-header">
+						The <strong>next generation</strong> of blockchain for business. Proven, flexible and built to run on <strong>any</strong> cloud. Deploy the leading
+						Hyperledger Fabric platform in the environment that’s right for <strong>your</strong> enterprise.
+					</h4>
 					<div className="project--overview">
 						<div className="project-overview-item">
 							<h5>Objective</h5>
@@ -124,26 +160,62 @@ const BlockchainPage = () => {
 							<h5>Tools</h5>
 							<Text>
 								<span className="tool-item">
-									<img src={reactlogo} style={{width: '1.25rem', marginRight: '.5rem'}} alt="React logo" aria-label="React logo" />React
+									<img
+										src={reactlogo}
+										style={{
+											width: '1.25rem',
+											marginRight: '.5rem',
+										}}
+										alt="React logo"
+										aria-label="React logo"
+									/>
+									React
 								</span>
 								<span className="tool-item">
-									<span style={{marginRight: '.5rem'}} role="img" aria-label="Crane emoji for Front end development">🎣</span>React hooks
+									<span style={{ marginRight: '.5rem' }}
+role="img"
+aria-label="Crane emoji for Front end development">
+										🎣
+									</span>
+									React hooks
 								</span>
 								<span className="tool-item">
-									<img src={reduxlogo} style={{width: '1.25rem', marginRight: '.5rem'}} alt="Redux logo" aria-label="Redux logo" />Redux
+									<img src={reduxlogo}
+style={{ width: '1.25rem', marginRight: '.5rem' }}
+alt="Redux logo"
+aria-label="Redux logo" />
+									Redux
 								</span>
 								<span className="tool-item">
-									<img src={sasslogo} style={{width: '1.25rem', marginRight: '.5rem'}} alt="Sass logo" aria-label="Sass logo" />Saas
+									<img src={sasslogo}
+style={{ width: '1.25rem', marginRight: '.5rem' }}
+alt="Sass logo"
+aria-label="Sass logo" />
+									Saas
 								</span>
 								<span className="tool-item">
-									<span style={{marginRight: '.5rem'}} role="img" aria-label="Artist palette emoji for Carbon Design system">🎨</span>
-									<GradientLink paddingBottom block external url="https://www.carbondesignsystem.com">
+									<span style={{ marginRight: '.5rem' }}
+role="img"
+aria-label="Artist palette emoji for Carbon Design system">
+										🎨
+									</span>
+									<GradientLink paddingBottom
+block
+external
+url="https://www.carbondesignsystem.com">
 										Carbon Design System
 									</GradientLink>
 								</span>
 								<span className="tool-item">
-									<img src={hyperledgerlogo} style={{width: '1.25rem', marginRight: '.5rem'}} alt="Hyperledger logo" aria-label="Hyperledger logo" />
-									<GradientLink paddingBottom flex external url="https://www.hyperledger.org/use/fabric" className="flex--link">
+									<img src={hyperledgerlogo}
+style={{ width: '1.25rem', marginRight: '.5rem' }}
+alt="Hyperledger logo"
+aria-label="Hyperledger logo" />
+									<GradientLink paddingBottom
+flex
+external
+url="https://www.hyperledger.org/use/fabric"
+className="flex--link">
 										Hyperledger Fabric
 									</GradientLink>
 								</span>
@@ -160,25 +232,106 @@ const BlockchainPage = () => {
 				<SectionMaxWidth>
 					<div className="project--section">
 						<h5>Target users</h5>
-						<Text medium margin>Our primary target users included enterprise clients from industries such as trade finance, global trade, food safety, identity management/digital identity, and many more.</Text>
-						<Text medium>Users can build solutions on their own through the use of the IBM Blockchain Platform, or they can choose to work with the IBM Cloud Garage who will help users to build a fully functioning prototype for their business use case, from design research and ideation through to technical implementation.</Text>
+						<Text medium
+margin>
+							Our primary target users included enterprise clients from industries such as trade finance, global trade, food safety, identity management/digital
+							identity, and many more.
+						</Text>
+						<Text medium>
+							Users can build solutions on their own through the use of the IBM Blockchain Platform, or they can choose to work with the IBM Cloud Garage who
+							will help users to build a fully functioning prototype for their business use case, from design research and ideation through to technical
+							implementation.
+						</Text>
 					</div>
 					<div className="project--section">
 						<h5>Objective</h5>
-						<Text medium margin>“IBM Blockchain is more than technology. It’s a movement to help you redefine your most important business relationships through trust, transparency and newfound collaboration.”</Text>
-						<Text medium margin>Traditionally, enterprise businesses have struggled with automating certain parts of their work and in most cases to the detriment of their users. The IBM Blockchain Platform is a technology which aims to solve that painpoint through a secured, permissioned, immutable transaction ledger.</Text>
-						<Text medium>The newest version of the <GradientLink external url="https://cloud.ibm.com/catalog/services/blockchain-platform">IBM Blockchain Platform</GradientLink> aims to meet you where you are, literally, allowing you to deploy the technology on the cloud of your choice. The only requirement is that you have a kubernetes cluster to host your components. You can even use a Red Hat OpenShift cluster to deploy your blockchain platform and as the leading Hyperledger Fabric platform this is one of the key reasons users choose the IBM Blockchain Platform to build their networks together, collaboration is easy and it just works.</Text>
+						<Text medium
+margin>
+							“IBM Blockchain is more than technology. It’s a movement to help you redefine your most important business relationships through trust,
+							transparency and newfound collaboration.”
+						</Text>
+						<Text medium
+margin>
+							Traditionally, enterprise businesses have struggled with automating certain parts of their work and in most cases to the detriment of their users.
+							The IBM Blockchain Platform is a technology which aims to solve that painpoint through a secured, permissioned, immutable transaction ledger.
+						</Text>
+						<Text medium>
+							The newest version of the{' '}
+							<GradientLink external
+url="https://cloud.ibm.com/catalog/services/blockchain-platform">
+								IBM Blockchain Platform
+							</GradientLink>{' '}
+							aims to meet you where you are, literally, allowing you to deploy the technology on the cloud of your choice. The only requirement is that you
+							have a kubernetes cluster to host your components. You can even use a Red Hat OpenShift cluster to deploy your blockchain platform and as the
+							leading Hyperledger Fabric platform this is one of the key reasons users choose the IBM Blockchain Platform to build their networks together,
+							collaboration is easy and it just works.
+						</Text>
 					</div>
 					<div className="project--section">
 						<h5>Process</h5>
-						<Text medium margin>My primary role on this project was contributing to and leading the front end development of the IBM Blockchain Platform. I was an integral part of our team’s decision to move over to React, in replace of the existing Angular app. This started by presenting all of the benefits we would have in switching, the much improved developer experience and defining a set of best practices that our team would follow regarding React development, standardizing application file/component structure, and guidelines as to how we would write our sass/css. As our application grew, building reusable components allowed us to work much faster and made it easier for developers on the team to focus on business level logic and not worry about building a brand new side panel for example. Not to mention the design consistency that precented further issues down the road.</Text>
-						<Text medium margin>As an IBM Cloud service, our product is required to use the <GradientLink external url="https://www.carbondesignsystem.com">Carbon design system</GradientLink>, IBM’s open source design system for all IBM digital experiences. I was responsible for ensuring that our team adhered to and properly implemented Carbon's <GradientLink external url="http://github.com/carbon-design-system/carbon/tree/master/packages/react">react components</GradientLink> and also led our migration to the Carbon 10 library version, which featured IBM’s new <GradientLink external url="http://ibm.com/design/language">design language guidelines</GradientLink>.</Text>
-						<Text medium margin>I have been able to also help interatively improve our user experience by helping run user research sessions and speaking with users at various events such as the IBM THINK conference, meetup events, IBM Blockchain Platform board of advisor meetings, and speaking directly with users to further understand how we can improve their user experience.</Text>
-						<Text medium>Another key experience that I delivered was an updated purchasing experience within IBM Cloud for the IBM Blockchain Platform. This included building a completely new micro-service within IBM Cloud for which blockchain customers use to purchase our product using the newly released <GradientLink external url="https://www.ibm.com/cloud/blog/announcements/enhanced-user-experience-patterns-in-ibm-cloud">user experience pattern library</GradientLink> for IBM Cloud. One of the painpoints this new experience solved was being able to create your kubernetes cluster while simultaneously creating your blockchain service. Through user research and NPS feedback, this was a point in our experience that caused confusion and was difficult for user’s to piece together the steps they needed to take. Finally, with this new service creation experience, there is no more context switching and you can do everything you need to do to create you blockchain service and the required prerequisites all from one place. My responsibilities on this project included building the React front end (using react hooks <span role="img" aria-label="Hook emoji for react hooks">🎣</span>) as well as the Node backend.</Text>
+						<Text medium
+margin>
+							My primary role on this project was contributing to and leading the front end development of the IBM Blockchain Platform. I was an integral part
+							of our team’s decision to move over to React, in replace of the existing Angular app. This started by presenting all of the benefits we would have
+							in switching, the much improved developer experience and defining a set of best practices that our team would follow regarding React development,
+							standardizing application file/component structure, and guidelines as to how we would write our sass/css. As our application grew, building
+							reusable components allowed us to work much faster and made it easier for developers on the team to focus on business level logic and not worry
+							about building a brand new side panel for example. Not to mention the design consistency that precented further issues down the road.
+						</Text>
+						<Text medium
+margin>
+							As an IBM Cloud service, our product is required to use the{' '}
+							<GradientLink external
+url="https://www.carbondesignsystem.com">
+								Carbon design system
+							</GradientLink>
+							, IBM’s open source design system for all IBM digital experiences. I was responsible for ensuring that our team adhered to and properly
+							implemented Carbon&apos;s{' '}
+							<GradientLink external
+url="http://github.com/carbon-design-system/carbon/tree/master/packages/react">
+								react components
+							</GradientLink>{' '}
+							and also led our migration to the Carbon 10 library version, which featured IBM’s new{' '}
+							<GradientLink external
+url="http://ibm.com/design/language">
+								design language guidelines
+							</GradientLink>
+							.
+						</Text>
+						<Text medium
+margin>
+							I have been able to also help interatively improve our user experience by helping run user research sessions and speaking with users at various
+							events such as the IBM THINK conference, meetup events, IBM Blockchain Platform board of advisor meetings, and speaking directly with users to
+							further understand how we can improve their user experience.
+						</Text>
+						<Text medium>
+							Another key experience that I delivered was an updated purchasing experience within IBM Cloud for the IBM Blockchain Platform. This included
+							building a completely new micro-service within IBM Cloud for which blockchain customers use to purchase our product using the newly released{' '}
+							<GradientLink external
+url="https://www.ibm.com/cloud/blog/announcements/enhanced-user-experience-patterns-in-ibm-cloud">
+								user experience pattern library
+							</GradientLink>{' '}
+							for IBM Cloud. One of the painpoints this new experience solved was being able to create your kubernetes cluster while simultaneously creating
+							your blockchain service. Through user research and NPS feedback, this was a point in our experience that caused confusion and was difficult for
+							user’s to piece together the steps they needed to take. Finally, with this new service creation experience, there is no more context switching and
+							you can do everything you need to do to create you blockchain service and the required prerequisites all from one place. My responsibilities on
+							this project included building the React front end (using react hooks{' '}
+							<span role="img"
+aria-label="Hook emoji for react hooks">
+								🎣
+							</span>
+							) as well as the Node backend.
+						</Text>
 					</div>
 					<div className="video--wrapper">
-						<video ref={videoRef} playsInline loop autoPlay muted className="ibp--video">
-							<source src={ibpVideo} type="video/mp4" />
+						<video ref={videoRef}
+playsInline
+loop
+autoPlay
+muted
+className="ibp--video">
+							<source src={ibpVideo}
+type="video/mp4" />
 						</video>
 						<button
 							type="button"
@@ -188,7 +341,8 @@ const BlockchainPage = () => {
 							className="video--controls"
 							onClick={toggleVideo}
 						>
-							<ToggleIcon hovering={hoveringVideoIcon} paused={paused} />
+							<ToggleIcon hovering={hoveringVideoIcon}
+paused={paused} />
 						</button>
 					</div>
 				</SectionMaxWidth>
